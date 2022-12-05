c49f8167b940629554a2127e51092170e0b80559
Alicia <garlic1124@gmail.com>
2022-12-06 04:13:23 +0800

    1206update

---
+++ dist/assets/ContactView.f55fb286.js
@@ -0,0 +1,1 @@
+import{_ as t,o as c,c as o,a as e}from"./index.7a742f17.js";const s={},n={class:"container mx-auto pt-20"},a=e("h1",null,"Contact",-1),_=e("div",{class:"h-screen"},null,-1),r=[a,_];function i(d,l){return c(),o("div",n,r)}var f=t(s,[["render",i]]);export{f as default};
